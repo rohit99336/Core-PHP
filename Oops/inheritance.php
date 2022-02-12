9b7ad3e3e037385304af32e3afa4bad770258047
rohit99336 <rohit99336@gmail.com>
2022-02-12 22:07:56 +0530

    inheritance

--- Oops/inheritance.php
+++ Oops/inheritance.php
@@ -7,22 +7,39 @@ class TV
 
     public function Display()
     {
-        echo "TV model: {$this->model} and size is: {$this->size}.";
+        echo "TV model: {$this->model} and size is: {$this->size}. <br>";
     }
 
     function __construct($a,$b)
     {
         $this->model = $a;
         $this->size = $b;
-        $this->Display();
+        echo "This is parent cunstructor <br>";
+        //$this->Display();
     }
 }
 
 class SmartTV extends TV
 {
-    
+    public function message($speed,$model) {
+        echo "Am I a smart tv or normal tv? <br>";
+        //  Call protected method from within derived class - OK
+         $this -> Display();  //call parent class member function
+         TV::__construct($speed,$model);  // call parent cunstructor
+         $this -> Display();
+    }
+ 
+    function __construct($j,$k)
+    {
+        echo "This is child cunstructor <br>";
+        TV::__construct($j,$k);
+        $this -> Display();  //call parent class member function
+
+    }
+     
 }
 
-$obj = new TV('LG','56')
+$obj = new SmartTV('LG','58');
+$obj->message('LG','G5');
 
 ?>
